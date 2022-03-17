d4b3b7ba2678cb2bbe878a317b8e579a7b4898f8
Btrujillo20 <Benjamin.Trujillo1@outlook.com>
2022-03-17 12:54:57 -0700

    edit

--- GUI-PING.ps1
+++ GUI-PING.ps1
@@ -29,12 +29,63 @@ $ping.location = New-Object system.drawing.point(110,40)
 $ping.AutoSize = $true
 $main_form.controls.add($ping)
 
-
 $button = New-Object System.Windows.Forms.Button
 $button.location = New-Object system.drawing.size(440,10)
 $button.size = New-Object system.drawing.size(120,23)
 $button.text = 'Send'
 $main_form.controls.add($button)
 
-$button.add_click({$ping.text = (test-connection combobox.text -quiet -count 1)})
-$main_form.ShowDialog()
+$button.add_click({$ping.text = (test-connection $combobox.text -quiet -count 1)
+
+
+if ($ping.text -eq 'false'){
+    $main_form.Close()
+    }})
+$main_form.ShowDialog() | Out-Null
+
+
+$main_form2 = New-Object System.Windows.Forms.Form
+$main_form2.Text = 'Trace Test'
+$main_form2.Width = 600
+$main_form2.Height = 400
+$main_form2.AutoSize = $true
+
+$label2 = new-object System.Windows.Forms.Label
+$label2.Text = "Website"
+$label2.location = New-Object system.drawing.point(0,20)
+$label2.autosize = $true
+$main_form2.Controls.Add($label2)
+
+$label3 = new-object System.Windows.Forms.Label
+$label3.Text = $combobox.Text
+$label3.location = New-Object system.drawing.point(100,20)
+$label3.autosize = $true
+$main_form2.Controls.Add($label3)
+
+$label4 = new-object System.Windows.Forms.Label
+$label4.Text = 'Would you like to perform a traceroute?'
+$label4.location = New-Object system.drawing.point(0,40)
+$label4.autosize = $true
+$main_form2.Controls.Add($label4)
+
+$trace = New-Object System.Windows.Forms.Label
+$trace.text = ''
+$trace.location = New-Object system.drawing.point(310,40)
+$trace.AutoSize = $true
+$main_form2.controls.add($trace)
+
+$yes = New-Object System.Windows.Forms.Button
+$yes.location = New-Object system.drawing.size(200,400)
+$yes.size = New-Object system.drawing.size(120,23)
+$yes.text = 'Yes'
+$main_form2.controls.add($yes)
+
+$nope = New-Object System.Windows.Forms.Button
+$nope.location = New-Object system.drawing.size(350,400)
+$nope.size = New-Object system.drawing.size(120,23)
+$nope.text = 'No'
+$main_form2.controls.add($nope)
+
+$yes.add_click({$trace.text = (test-netconnection -traceroute -InformationLevel Quiet $combobox.text)})
+
+$main_form2.showdialog() | out-null
